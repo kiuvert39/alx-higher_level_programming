8dd299a8a0bf4de02c77b2bb4d478365065e82b4
kiuvert39 <forrexplug@gmail.com>
2024-03-16 15:00:05 +0100

    magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,10 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-    return 98 + a ** b
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ('add', 'sub'), 0)
+    
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
