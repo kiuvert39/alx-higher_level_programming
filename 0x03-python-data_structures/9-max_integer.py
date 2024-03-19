dbd2cb04d28f43e832ab9a30cc632c70e8aae1e2
kiuvert39 <forrexplug@gmail.com>
2024-03-19 18:14:29 +0100

    max number of list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 def max_integer(my_list=[]):
-    pass
+    if len(my_list) <= 0:
+        return (None)
+    else:
+        maximum = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > maximum:
+                maximum = my_list[i]
+        return maximum
