47234f39648c0c4288feb29027b03ab068746b5e
kiuvert39 <forrexplug@gmail.com>
2024-03-19 06:03:33 +0100

    displaying a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=' ' if j != i[-1] else ' ')
-        print()
+    if matrix:
+        for i in matrix:
+            for j in i:
+                print("{:d}".format(j), end=' ' if j != i[-1] else ' ')
+            print()
