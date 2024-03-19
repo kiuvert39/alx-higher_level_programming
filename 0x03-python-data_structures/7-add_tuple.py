708bdac8b4022867a47f3a5db12e7c61ee629d73
kiuvert39 <galvinegbe@gmail.com>
2024-03-19 00:40:48 -0700

    errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure tuples have at least 2 elements
     tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
     tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
-    
-    # Perform addition
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
