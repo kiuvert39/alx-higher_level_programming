5bf9840bbdb9b984a34b0cc8596eec869d93b8d0
kiuvert39 <forrexplug@gmail.com>
2024-03-19 18:36:03 +0100

    printing matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in matrix:
-            for j in i:
-                print("{:d}".format(j), end=' ' if j != i[-1] else '')
-            print()
+    if not matrix:
+        print()
+    else:
+        for row in range(len(matrix)):
+            for item in range(len(matrix[row])):
+                if item != len(matrix[row]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[row][item]), end=endspace)
+            print
