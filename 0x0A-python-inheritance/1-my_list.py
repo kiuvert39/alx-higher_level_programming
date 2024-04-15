447e3bbdad6f4df90e252754e9c4a13a8f8349b3
kiuvert39 <forrexplug@gmail.com>
2024-04-15 11:42:01 +0100

    sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+'''Module for MyList class.'''
+
+
 class MyList(list):
     """this class   inherite  from  the list    class"""
-    def print_sorted(self): 
+    def print_sorted(self):
         """this method  prints  a sorted list"""
         print(sorted(self))
-       
