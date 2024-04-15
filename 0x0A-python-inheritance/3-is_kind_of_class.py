005b79ada91f2527d3ee733d03a691f569adc6ec
kiuvert39 <forrexplug@gmail.com>
2024-04-15 12:20:31 +0100

    instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''function for instance  of a class.'''
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class.
+
+    Args:
+    obj: The object to check.
+    cls: The class to check against.
+
+    Returns:
+    True if the object is an instance of the specified class or its subclass, False otherwise.
+    """
+    return isinstance(obj, a_class)
