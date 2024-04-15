19ca3ad2a75c9ccfaaf75845e61796598a5f1639
kiuvert39 <forrexplug@gmail.com>
2024-04-15 11:23:10 +0100

    sorted

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+def lookup(obj):
+
+    """ this function returns the methods, attribute in  an  object
+        Args:
+            obj:  this the actual object  that contain the attribute and the methods 
+    """ 
+
+    return  (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,6 @@
+class MyList(list):
+    """this class   inherite  from  the list    class"""
+    def print_sorted(self): 
+        """this method  prints  a sorted list"""
+        print(sorted(self))
+       
