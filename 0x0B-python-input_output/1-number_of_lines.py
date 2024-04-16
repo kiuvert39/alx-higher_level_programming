860487c6d1a0567e32c1cb825442070901ae365b
kiuvert39 <forrexplug@gmail.com>
2024-04-16 20:39:41 +0100

    fixe read

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
 def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+    """ function that returns the number of lines of a text file """
+
+    with open(filename) as f:
+        count = len(f.readlines())
+    return count
