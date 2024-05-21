80458af5cf1f3b3d479c97c88282597fed75d0cc
kiuvert39 <forrexplug@gmail.com>
2024-05-21 19:13:50 +0100

    inserting

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- This SQL query retrieves the all the data in a table
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- This SQL query retrieves the all the data in a table
+INSERT INTO `first_table` (`id`,`name`) VALUES(89, `Best School`)
