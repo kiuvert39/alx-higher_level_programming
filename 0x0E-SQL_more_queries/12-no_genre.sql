30301c07245f1c42cf9a9336b5c24ddb69584d25
kiuvert39 <forrexplug@gmail.com>
2024-05-22 20:18:19 +0100

    files

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- 12-no_genre.sql
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- 14-my_genres.sql
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
