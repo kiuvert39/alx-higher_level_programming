0f5f087d5dd14d7a20350a76932830d9108b25a1
kiuvert39 <forrexplug@gmail.com>
2024-07-09 19:27:00 +0100

    fetching data

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
