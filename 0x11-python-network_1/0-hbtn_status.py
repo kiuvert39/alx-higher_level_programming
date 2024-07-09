e662cc60898e94a4b3165988de6e71e6ccb1b55c
kiuvert39 <forrexplug@gmail.com>
2024-07-09 19:53:49 +0100

    geting the header of a request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     body = response.read()
 
-    print("Body response:")
+    rint("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(body.decode('utf-8')))
